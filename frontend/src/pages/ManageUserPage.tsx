05e4f9f3411b1c97077447f352f902e33acb167f
huekoh <huexkoh@gmail.com>
2026-03-13 09:58:00 +0800

    feat: create manage active rooms page

--- frontend/src/pages/ManageUserPage.tsx
+++ frontend/src/pages/ManageUserPage.tsx
@@ -1,7 +1,6 @@
 import { Box } from "@mui/material";
 import AdminSideMenu from "../features/admin/AdminSideMenu";
 import AdminTable from "../features/admin/AdminTable";
-import AdminTableAddButton from "../components/RoundedFilledButton";
 import SearchBar from "../components/SearchBar";
 
 const tableFields = [
@@ -14,19 +13,13 @@ const tableFields = [
   "QuestionsCompleted",
 ];
 
-function UserDirectoryPage() {
+function ManageUserPage() {
   return (
     <Box sx={{ display: "flex", width: "100vw", height: "100vh" }}>
       <AdminSideMenu />
-      <AdminTable
-        tableButtons={[
-          <SearchBar />,
-          <AdminTableAddButton label={"Add User"} />,
-        ]}
-        tableFields={tableFields}
-      />
+      <AdminTable tableButtons={[<SearchBar />]} tableFields={tableFields} />
     </Box>
   );
 }
 
-export default UserDirectoryPage;
+export default ManageUserPage;
--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -7,9 +7,10 @@ import {
 import SignUp from "../pages/SignUpPage";
 import Login from "../pages/LoginPage";
 import UserHomePage from "../pages/UserHomePage";
-import UserDirectoryPage from "../pages/UserDirectoryPage";
+import ManageUserPage from "../pages/ManageUserPage";
 import ManageAdminPage from "../pages/ManageAdminPage";
-import ManageQuestionPage from "../pages/QuestionBankPage";
+import ManageQuestionPage from "../pages/ManageQuestionPage";
+import ManageRoomPage from "../pages/ManageRoomPage";
 
 function AppRouter() {
   return (
@@ -21,11 +22,13 @@ function AppRouter() {
 
         <Route path="/home" element={<UserHomePage />} />
 
-        <Route path="/admin/user-directory" element={<UserDirectoryPage />} />
+        <Route path="/admin/manage-user" element={<ManageUserPage />} />
 
         <Route path="/admin/manage-admin" element={<ManageAdminPage />} />
 
         <Route path="/admin/manage-question" element={<ManageQuestionPage />} />
+
+        <Route path="/admin/manage-room" element={<ManageRoomPage />} />
 
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
