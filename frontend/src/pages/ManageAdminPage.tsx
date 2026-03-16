7bd8b0b7f03de8a34e1b2c9a754f029856c7c24b
huekoh <huexkoh@gmail.com>
2026-03-16 18:54:27 +0800

    feat: display admin accounts with fetched data from user service

--- frontend/src/pages/ManageAdminPage.tsx
+++ frontend/src/pages/ManageAdminPage.tsx
@@ -1,19 +1,29 @@
-import { Box } from "@mui/material";
+import { Box, TableCell, TableRow } from "@mui/material";
 import AdminSideMenu from "../features/admin/AdminSideMenu";
 import AdminTable from "../features/admin/AdminTable";
 import AdminTableAddButton from "../components/RoundedFilledButton";
 import SearchBar from "../components/SearchBar";
-
-const tableFields = [
-  "#",
-  "Full Name",
-  "Email",
-  "Role",
-  "Joined Date",
-  "Status",
-];
+import useAdmins from "../hooks/useAdmin";
+import { useEffect } from "react";
 
 function ManageAdminPage() {
+  const tableFields = [
+    "#",
+    "User Name",
+    "Email",
+    "Role",
+    "Joined Date",
+    "Status",
+  ];
+
+  const roles = { "1": "User", "2": "Admin", "3": "Super Admin" };
+
+  const { admins, isLoading, error, loadAdmins, page } = useAdmins();
+
+  useEffect(() => {
+    loadAdmins();
+  }, []);
+
   return (
     <Box sx={{ display: "flex", width: "100vw", height: "100vh" }}>
       <AdminSideMenu />
@@ -23,6 +33,21 @@ function ManageAdminPage() {
           <AdminTableAddButton label={"Add Admin"} />,
         ]}
         tableFields={tableFields}
+        rows={admins}
+        isLoading={isLoading}
+        error={error}
+        renderRow={(admin, index) => (
+          <TableRow key={admin.userId} hover sx={{ width: "100%" }}>
+            <TableCell>{(page - 1) * 10 + index + 1}</TableCell>
+            <TableCell>{admin.userName}</TableCell>
+            <TableCell>{admin.email}</TableCell>
+            <TableCell>{roles[admin.role]}</TableCell>
+            <TableCell>
+              {new Date(admin.createdAt).toLocaleDateString()}
+            </TableCell>
+            <TableCell>(Coming Soon)*</TableCell>
+          </TableRow>
+        )}
       />
     </Box>
   );
