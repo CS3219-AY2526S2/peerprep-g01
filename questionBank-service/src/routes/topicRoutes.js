eebd7dfcc16c10bdc48832d7efa33d8297ea97fd
huekoh <huexkoh@gmail.com>
2026-04-16 02:34:32 +0800

    fix: add / prefix to new endpoint's route

--- questionBank-service/src/routes/topicRoutes.js
+++ questionBank-service/src/routes/topicRoutes.js
@@ -5,7 +5,7 @@ const topicController = require("../controllers/topicController");
 
 //topic table api
 router.get("/topic", topicController.getAllTopics);
-router.get("topic/difficulty", topicController.getAllTopicsWithDifficulty);
+router.get("/topic/difficulty", topicController.getAllTopicsWithDifficulty);
 router.post("/topic", topicController.addTopic);
 router.delete("/topic/:id", topicController.deleteTopic);
 
